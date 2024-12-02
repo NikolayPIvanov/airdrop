9e568e84e27477e31b4c2274dc9651f7c8cd9e52
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-12-02 20:49:46 +0200

    Adding mint

--- src/PurToken.sol
+++ src/PurToken.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract PurToken is ERC20 {
-    constructor() ERC20("PurToken", "Pur") {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PurToken is ERC20, Ownable {
+    constructor() ERC20("PurToken", "Pur") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 value) external virtual onlyOwner {
+        _mint(to, value);
+    }
 }
