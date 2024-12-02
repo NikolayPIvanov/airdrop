7e74b04982928e936ad3bc482d41a2a8f5fd2ca7
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-12-02 20:47:47 +0200

    ERC-20

---
+++ src/MerkeAirdrop.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract MerkleAirdrop {
+// some list of addresses
+// allow someone in the list to claim token
+}
---
+++ src/PurToken.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract PurToken is ERC20 {
+    constructor() ERC20("PurToken", "Pur") {}
+}
