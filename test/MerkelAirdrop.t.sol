221d09d44c226fc3e0a1268a6b8fff28cf22ec8e
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-12-03 06:50:48 +0200

    Adding generate scripts

---
+++ test/MerkelAirdrop.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract MerkelAirdropTest is Test {
+    function setUp() public {
+        console.log("setUp");
+    }
+}
